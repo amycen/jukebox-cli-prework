3e5bb5b3f3ad8d2970da0c9f4cee852c5aead109
Amy Cen <amy.cen@blackrock.com>
2019-04-19 01:05:04 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -26,4 +26,26 @@ def list(my_songs)
   end
 end
 
-list(my_songs)
+
+def play(my_songs)
+  #this method is slightly different!
+  #you should still ask the user for input and collect their song choice
+  #this time, only allow user's to input a song name
+  #check to see if the name they give is in fact a key of the my_songs hash
+  #if it isn't, tell them their choice is invalid
+  #if it is, play the song using the system 'open <file path>' syntax
+  #get the file path of the song by looking it up in the my_songs hash
+  puts "Please enter a song name:"
+  song_name = gets.chomp
+  if my_songs.include? (song_name)
+    system open song_name
+end
+
+def exit_jukebox
+  #this method is the same as in jukebox.rb
+end
+
+def run(my_songs)
+  #this method is the same as in jukebox.rb
+end
+
