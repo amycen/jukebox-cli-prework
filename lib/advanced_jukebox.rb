6ecb0c5c5628fb791e9f5abb962788e6ff773e65
Amy Cen <amy.cen@blackrock.com>
2019-04-19 00:59:20 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -21,7 +21,7 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
-  my_songs.each.with_index(1) |name, path, index|
+  my_songs.each.with_index(1) |(name, path), index|
     puts "#{index}. #{name}"
 end
 
